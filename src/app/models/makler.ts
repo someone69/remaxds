232405ad8d09e64231a6cb29ecc75182141bad93
Someone69 <peter.nrancsik@gmail.com>
2018-02-02 13:41:54 +0100

    adding interface

---
+++ src/app/models/makler.ts
@@ -0,0 +1,12 @@
+export interface Makler {
+    firstName: string,
+    lastName: string,
+    position: string,
+    tel: string,
+    email: string,
+    web: string,
+    bio:{
+        sk: string,
+        hu: string
+    }
+}
